85dd5b9aa48e4b67979cf56a990bc7bea5450592
Lars Hellemo <hellemo@gmail.com>
2023-12-03 16:35:46 +0100

    Optimizations

--- 2023/src/day_03.jl
+++ 2023/src/day_03.jl
@@ -6,31 +6,16 @@ input = "2023/data/day_03.txt"
 testinput = "2023/data/test_03.txt"
 
 function parse_data(input)
-    D = split(chomp(read(input, String)), "\n")
-    M = length(D)
-    N = length(first(D))
-    A = Array{Char}(undef, M, N)
-    for i = 1:M, j = 1:N
-        A[i, j] = D[i][j]
-    end
-    return rotr90(A)
+    stack(readlines(input); dims = 2)
 end
 
-function neighbours()
-    [
-        CartesianIndex(0, -1),
-        CartesianIndex(-1, 0),
-        CartesianIndex(1, 0),
-        CartesianIndex(0, 1),
-        CartesianIndex(-1, -1),
-        CartesianIndex(-1, 1),
-        CartesianIndex(1, 1),
-        CartesianIndex(1, -1),
-    ]
-end
+const neighbours =
+    let N = [i for i in CartesianIndices((-1:1, -1:1)) if i != CartesianIndex(0, 0)]
+        N
+    end
 
 function legal_moves(A, p::CartesianIndex)
-    return (p + a for a in neighbours() if checkbounds(Bool, A, p + a))
+    return (p + a for a in neighbours if checkbounds(Bool, A, p + a))
 end
 
 function validpn(A, candidate)
@@ -56,8 +41,7 @@ end
 function solve(A)
     ispn = false
     candidates = Vector{CartesianIndex}[] # arrays of CIs
-    candidate =  CartesianIndex[] #CIs
-    # gears = Dict{CartesianIndex,Vector{CartesianIndex}}()
+    candidate = CartesianIndex[] #CIs
     gears = Dict{CartesianIndex,Vector{Vector{CartesianIndex}}}()
     count = 0
     for i in CartesianIndices(A)
@@ -108,9 +92,9 @@ function part_2(input)
     _, gears = solve(A)
 
     sumgears = 0
-    for (k, v) in gears
-        if length(v) == 2
-            sumgears = sumgears + pn(A, first(v)) * pn(A, last(v))
+    for g in values(gears)
+        if length(g) == 2
+            sumgears = sumgears + pn(A, first(g)) * pn(A, last(g))
         end
     end
     return sumgears
