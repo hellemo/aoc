5f0d0e7a5a0b2edf758247b08880cf91604a94ff
Lars Hellemo <hellemo@gmail.com>
2023-12-07 15:30:36 +0100

    December 8

---
+++ 2023/src/day_08.jl
@@ -0,0 +1,65 @@
+# https://adventofcode.com/2023/day/8
+using AdventOfCode
+using Base.Threads
+using Test
+
+input = "2023/data/day_08.txt"
+testinput = "2023/data/test_08.txt"
+
+function parse_data(input)
+    instructions, rest = Iterators.peel(eachline(input))
+    d = Dict{String,@NamedTuple{L::SubString{String}, R::SubString{String}}}()
+    for l in rest
+        if length(l) > 0
+            A, L, R = parse_line(l)
+            d[A] = (; L, R)
+        end
+    end
+    return instructions, d
+end
+
+function parse_line(l)
+    rx = r"(?<address>[A-Z]*) = \((?<L>[A-Z]*), (?<R>[A-Z]*)\)"
+    m = match(rx, l)
+    return Tuple(m.captures)
+end
+
+function solve(instructions, d, start = "AAA", stop = "ZZZ")
+    location = start
+    steps = 0
+    while !endswith(location, stop)
+        for c in instructions
+            steps = steps + 1
+            if c == 'L'
+                location = first(d[location])
+            else
+                location = last(d[location])
+            end
+        end
+    end
+    return steps
+end
+
+function part_1(input)
+    solve(parse_data(input)...)
+end
+
+@test part_1(testinput) == 2
+@test part_1(input) == 19631
+@info "Part1:" part_1(input)
+
+function part_2(input)
+    i, d = parse_data(input)
+    locations = [string(l) for l in filter(endswith("A"), keys(d))]
+    steps = fetch.(@spawn solve(i,d,l,"Z") for l in locations)
+    f = [gcd(steps[1], steps[i]) for i = 2:length(steps)]
+    f1 = 1
+    if allequal(f)
+        steps ./= first(f)
+        f1 = first(f)
+    end
+    return prod(steps) * f1
+end
+
+@test part_2(input) == 21003205388413
+@info "Part2:" part_2(input)
